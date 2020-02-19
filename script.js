483b952d3c5e5dfdb6fb94f80f7cf2eabcf83e04
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-02-18 16:01:47 -0800

    Created fetch function

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function $testAlert() {
-    alert("This is a test");
+const randomUser = "https://randomuser.me/api/";
+
+async function fetchData(url) {
+    try {
+        for (let i = 0; i < 12; i++) {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log([data]);
+        }
+    } catch (error) {
+        throw error;
+    }
 }
 
-$testAlert();
+fetchData(randomUser);

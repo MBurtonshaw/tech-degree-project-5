010f6b9171bfa5f268efda9d42eb4e5740fefdba
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-02-20 16:31:41 -0800

    Added card divs; trying to loop through them
    Added a card div for each JSON result; trying to loop through them so I can add further HTML tags to them.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
-const randomUser = "https://randomuser.me/api/";
+const randomUser = "https://randomuser.me/api/?results=12";
 
-async function fetchData(url) {
-    try {
-        for (let i = 0; i < 12; i++) {
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log([data]);
-        }
-    } catch (error) {
-        throw error;
-    }
+function $getJSON() { 
+    $.ajax({
+    url: randomUser,
+    dataType: "json",
+    success: data => {
+        const results = data.results;
+        const resultsArray = [results];
+        //console.log(results);
+        results.map(person => { 
+            $("#gallery").append("<div class='card'></div>");
+        })           
+    }             
+})
+};
+
+$getJSON();
+
+const cardDivs = document.getElementsByClassName("card");
+console.log(cardDivs);
+
+for (let i = 0; i < cardDivs.length; i++) {
+    console.log('anything');
 }
 
-fetchData(randomUser);
+    /*$(".card-img-container")[i].append("<img class='card-img' src='https://placehold.it/90x90' alt='profile picture'>");
+    $(".card").append("<div class='card-info-container'></div>");
+    $(".card-info-container").append("<h3 id='name' class='card-name cap'>first last</h3>");
+    $(".card-info-container").append("<p id='card_1' class='card-text'>email</p>");
+    $(".card-info-container").append("<p id='card_2' class='card-text cap'>city, state</p>");
+    $("#name").text(person.name.first + " " + person.name.last);
+    $("#card_1").text(person.email);
+    $("#card_2").text(person.location.city + ", " + person.location.state);
+    $(".card-img").attr("src", person.picture.thumbnail);  */
+
+

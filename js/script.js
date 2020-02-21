7e7172c9db09f897147b9e2f785e1e95aaca014e
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-02-20 16:38:07 -0800

    Aesthetic revision

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 const randomUser = "https://randomuser.me/api/?results=12";
 
-function $getJSON() { 
+function $getJSON() {
     $.ajax({
-    url: randomUser,
-    dataType: "json",
-    success: data => {
-        const results = data.results;
-        const resultsArray = [results];
-        //console.log(results);
-        results.map(person => { 
-            $("#gallery").append("<div class='card'></div>");
-        })           
-    }             
-})
-};
+        url: randomUser,
+        dataType: "json",
+        success: data => {
+            const results = data.results;
+            //console.log(results);
+            results.forEach(person => {
+                $("#gallery").append("<div class='card'></div>");
+            });
+        }
+    });
+}
 
 $getJSON();
 
@@ -21,10 +20,10 @@ const cardDivs = document.getElementsByClassName("card");
 console.log(cardDivs);
 
 for (let i = 0; i < cardDivs.length; i++) {
-    console.log('anything');
+    console.log("anything");
 }
 
-    /*$(".card-img-container")[i].append("<img class='card-img' src='https://placehold.it/90x90' alt='profile picture'>");
+/*$(".card-img-container").append("<img class='card-img' src='https://placehold.it/90x90' alt='profile picture'>");
     $(".card").append("<div class='card-info-container'></div>");
     $(".card-info-container").append("<h3 id='name' class='card-name cap'>first last</h3>");
     $(".card-info-container").append("<p id='card_1' class='card-text'>email</p>");
@@ -33,5 +32,3 @@ for (let i = 0; i < cardDivs.length; i++) {
     $("#card_1").text(person.email);
     $("#card_2").text(person.location.city + ", " + person.location.state);
     $(".card-img").attr("src", person.picture.thumbnail);  */
-
-

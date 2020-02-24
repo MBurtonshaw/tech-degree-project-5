c03ebf9567ed3b3eab48e51780d57ab499ac00e6
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-02-23 16:22:40 -0800

    Array created, data appended
    Created array to store data for later use.
    Data appended to divs, replacing placeholder text and appearing onscreen.
    Functions placed into first getJSON() function.

--- js/script.js
+++ js/script.js
@@ -1,57 +1,65 @@
 const randomUser = "https://randomuser.me/api/?results=12";
+let userArray = [];
 
+//This function fetches the JSON data, stores it in the userArray[], and also calls the personalData function to display the data on-screen, as well as the cardAppendage() function to append divs, etc. for formatting the data
 function getJSON(url) {
     fetch(url)
-    .then(response => response.json())
-    .then(json => {
-        const jsonData = json.results;
-        console.log(jsonData);
-        return jsonData;
-    })
+        .then(response => response.json())
+        .then(json => {
+            const jsonData = json.results;
+            userArray = [...jsonData];
+            personalData(userArray);
+        })
+        .then(cardAppendage());
 }
 
+//Invoked in getJSON() function; this function is to format the data, looping through to create and append divs with placeholders, etc. for each person, and to give the data somewhere to be displayed
+//Ran into some trouble at one point; the <div> was appending to $(".card) as text. I found this solution: https://stackoverflow.com/questions/40462236/jquery-appends-as-text-instead-of-html
 function cardAppendage() {
     for (let i = 0; i < 12; i++) {
         $("#gallery").append("<div class='card'></div>");
-        const $newCard = document.createElement("div");
+        $(".card")
+            .eq(i)
+            .append("<div class='card-img-container'></div>");
+        $(".card")
+            .eq(i)
+            .append("<div class='card-info-container'></div>");
+        $(".card-img-container")
+            .eq(i)
+            .append(
+                "<img class='card-img' src='https://placehold.it/90x90' alt='profile picture'>"
+            );
+        $(".card-info-container")
+            .eq(i)
+            .append("<h3 id='name' class='card-name cap'>first last</h3>");
+        $(".card-info-container")
+            .eq(i)
+            .append("<p class='card-text'>email</p>");
+        $(".card-info-container")
+            .eq(i)
+            .append("<p class='card-text cap'>city, state</p>");
     }
 }
 
+//Called within the getJSON() function; selects certain data to replace the placeholders used in cardAppendage() function
+function personalData(data) {
+    for (let k = 0; k < data.length; k++) {
+        $(".card-name")
+            .eq(k)
+            .text(data[k].name.first + " " + data[k].name.last);
+        $(".card-img")
+            .eq(k)
+            .attr("src", data[k].picture.large);
+        $("h3")
+            .next()
+            .eq(k)
+            .text(data[k].email);
+        $("p")
+            .next()
+            .eq(k)
+            .text(data[k].location.city);
+    }
+}
+
+//Calling on function
 getJSON(randomUser);
-cardAppendage();
-//console.log(getJSON(randomUser));
-//console.log(getJSON("https://randomuser.me/api/?results=12"));
-
-//The <div> was appending to $(".card) as text. I found this solution: https://stackoverflow.com/questions/40462236/jquery-appends-as-text-instead-of-html
-
-for (let j = 0; j < 12; j++) {
-    $(".card")
-        .eq(j)
-        .append("<div class='card-info-container'></div>");
-    $(".card")
-        .eq(j)
-        .append("<div class='card-img-container'></div>");
-    $(".card-img-container")
-        .eq(j)
-        .append(
-            "<img class='card-img' src='https://placehold.it/90x90' alt='profile picture'>"
-        );
-    $(".card-info-container")
-        .eq(j)
-        .append("<h3 id='name' class='card-name cap'>first last</h3>");
-    $(".card-info-container")
-        .eq(j)
-        .append("<p id='card_1' class='card-text'>email</p>");
-    $(".card-info-container")
-        .eq(j)
-        .append("<p id='card_2' class='card-text cap'>city, state</p>");
-}
-
-async function personalData(data) {
-    await console.log(data);
-    //$("#name").text(data.name.first + " " + data.name.last);
-    //$("#card_1").text(data.email);
-    //$("#card_2").text(data.city + ", " + data.state);
-    //$(".card-img").attr("src", data.thumbnail);
-}
-personalData(getJSON(randomUser)); 
